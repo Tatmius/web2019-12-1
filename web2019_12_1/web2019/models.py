f42df2879982dd6f241e1ac20a0b39ecb6114042
tatmius <kddiw.comusodayo@gmail.com>
2019-12-02 06:19:16 +0900

    I can't get how to reflect the text

--- web2019_12_1/web2019/models.py
+++ web2019_12_1/web2019/models.py
@@ -17,13 +17,14 @@ def csv_mod_read_row2(path, idx):
             line_num += 1
     return None
 
-
-number=random.randint(2,16772)
-print(number)
-#url=urlopen('http://pubserver2.herokuapp.com/api/v0.1/books/{}/content'.format(book_id))
-url=csv_mod_read_row2('Book1.csv', number)
-print(url)
-r=urlopen(url[0])
-soup = BeautifulSoup(r,"lxml",from_encoding="shift_jis")
-#main_text=soup.find("div","main_text")
-r.close()
+def random_scrape():
+    number=random.randint(2,16772)
+    print(number)
+    #url=urlopen('http://pubserver2.herokuapp.com/api/v0.1/books/{}/content'.format(book_id))
+    url=csv_mod_read_row2('Book1.csv', number)
+    print(url)
+    r=urlopen(url[0])
+    soup = BeautifulSoup(r,"lxml",from_encoding="shift_jis")
+    main_text=soup.find("div","main_text")
+    r.close()
+    return main_text
--- web2019_12_1/web2019/views.py
+++ web2019_12_1/web2019/views.py
@@ -8,7 +8,7 @@ import web2019.models as models
 def hello_template(request):
     return render(request, 'index.html')
 
-text=models.soup
+text=models.random_scrape()
 def viewFunction(request):
   template_name = "index.html"
   context = {"text" : text} # <- "text"がテンプレート内での変数名
