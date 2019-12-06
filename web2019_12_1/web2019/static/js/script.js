38131745800fdf77c720ca40e46bb994438e2ecc
tatmius <kddiw.comusodayo@gmail.com>
2019-12-06 17:13:41 +0900

    add reload buttom

--- web2019_12_1/web2019/static/js/script.js
+++ web2019_12_1/web2019/static/js/script.js
@@ -1,6 +1,11 @@
-document.getElementById("section02").style.display ="none";
+secion02=document.getElementById("section02");
+secion02.style.display ='none';
 
 function scrolldown() {
     getElementById("section02").style.display = 'block';
     window.scrollTo(0,100);
+}
+
+function reload(){
+   location.reload();
 }
