3da060dc6f495c96d4f2161b68a8d07b0c7f326d
tatmius <kddiw.comusodayo@gmail.com>
2019-12-06 16:23:58 +0900

    change buttom

--- web2019_12_1/web2019/static/js/script.js
+++ web2019_12_1/web2019/static/js/script.js
@@ -1,3 +1,6 @@
+document.getElementById("section02").style.display ="none";
+
 function scrolldown() {
+    getElementById("section02").style.display = 'block';
     window.scrollTo(0,100);
 }
--- web2019_12_1/web2019/views.py
+++ web2019_12_1/web2019/views.py
@@ -7,9 +7,8 @@ import web2019.models as models
 
 
 
-
-
 def viewFunction(request):
+  que=request.POST.get("pushed")
   template_name = "index.html"
   soup=models.random_scrape()
   mainText=models.getMainText(soup)
