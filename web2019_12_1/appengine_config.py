f92fa22be97f4853c10fb8c31c4f1b24cbf5e963
tatmius <kddiw.comusodayo@gmail.com>
2019-12-17 19:38:11 +0900

    delete appengine_config

--- web2019_12_1/appengine_config.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import logging
-
-ROOTPATH=os.path.dirname(__file__)
-EGGSPATH=os.path.join(ROOTPATH,'eggs')
-print(os.listdir(EGGSPATH))
-for egg in os.listdir(EGGSPATH):
-    sys.path.append(os.path.join(EGGSPATH,egg))
